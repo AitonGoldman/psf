ff7ab7e82b51d0c7ed5a04fd38aa68f9560e2f4a
aiton goldman <yanetut@gmail.com>
2015-11-26 19:01:43 +0000

    fixed score model and tests

--- test/model_challenge_score.js
+++ test/model_challenge_score.js
@@ -10,12 +10,48 @@ require('../config/test_database');
 require('../models/ChallengeScore');
 require('../models/ChallengeUserLogin');
 
+
 var ChallengeScore = mongoose.model('ChallengeScore');
 var ChallengeUserLogin = mongoose.model('ChallengeUserLogin');
+
 
 describe('ChallengeScore', function() {
     var fake_user_1
     var fake_user_2
+
+    var good_score = new ChallengeScore({/*winnerId: fake_user_1,*/
+	machinePlayerdOnId: '1234',
+	challenge: false,
+	season: 'pre-season',
+	scorePlayers: [{
+	    playerName: "poop",
+	    wins: 10,
+	    losses: 1,
+	    points: 55
+	},{
+	    playerName: "poop2",
+	    wins: 11,
+	    losses: 11,
+	    points: 15
+	}]
+    });
+    
+    var bad_score = new ChallengeScore({/*winnerId: fake_user_1,*/
+	machinePlayerdOnId: '1234',
+	scorePlayers: [{
+	    wins: 'aa',
+	    losses: 'bb',
+	    points: 'cc'
+	},{
+	    playerName: "poop2",
+	    wins: 'aa',
+	    losses: 'bb',
+	    points: 'cc'
+	}]
+    });
+    
+    
+    
     beforeEach(function(done) {
 	mockgoose.reset();
 	var user_1_info = {local: {email: 'hi@hi.com',
@@ -30,42 +66,30 @@ describe('ChallengeScore', function() {
 	var challengeuserlogin2 = new ChallengeUserLogin(user_2_info);	    
 	challengeuserlogin.save(function(err,user){	    
 	    fake_user_1 = user._id;
+	    good_score.winnerId=fake_user_1;
+	    good_score.scorePlayers[0].playerId=fake_user_1;
+	    good_score.scorePlayers[1].playerId=fake_user_1;
+	    bad_score.winnerId='notreal';
+	    bad_score.scorePlayers[0].playerId='notreal';
+	    bad_score.scorePlayers[1].playerId='notreal';
 	    challengeuserlogin2.save(function(err,user){	    
 		fake_user_2 = user._id;
-		console.log(fake_user_1+" "+fake_user_2)
 		done();
 	    })	    
 	})
     })
-     this.timeout(5000);
      it('valid score', function (done) {
- 	 var challengescore = new ChallengeScore({winnerId: fake_user_1,
-						  machinePlayerdOnId: '1234',
-						  challenge: false,
-						  scorePlayers: [{
-						      playerId: fake_user_1,
-						      playerName: "poop",
-						      winOrLose: true,
-						      wins: 10,
-						      losses: 1,
-						      points: 55
-						  }]
-						 });
-	 // challengescore.scorePlayers.push([{
-	 //     playerId: fake_user_1,
-	 //     playerName: "poop",
-	 //     winOrLose: true,
-	 //     wins: 10,
-	 //     losses: 1,
-	 //     points: 55
-	 // }])
- 	 // //return challengeuserlogin.save().should.be.fulfilled;
- 	 challengescore.save(function(err,score){
-	 //     user.should.not.be.null
-	     console.log("hi there"+score)
+ 	 good_score.save(function(err,score){
+	     score.should.not.be.null
 	     done()
-	 })
-	 
+	 })	 
      });
+     it('invalid score', function (done) {
+ 	 bad_score.save(function(err,score){
+	     err.should.not.be.null
+	     done()
+	 })	 
+     });
+
 });
 
