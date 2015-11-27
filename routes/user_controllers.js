4844287786c7d363b9377e19a30cf645e3a4f2d6
aiton goldman <yanetut@gmail.com>
2015-11-27 21:21:45 +0000

    passwords now hashed

--- routes/user_controllers.js
+++ routes/user_controllers.js
@@ -15,6 +15,7 @@ module.exports = function(login_type){
 	controllers = {
 	    addUser: function(req,res,next){
 		var challengeuserlogin = new ChallengeUserLogin(req.body);
+		challengeuserlogin.local.password = challengeuserlogin.generateHash(challengeuserlogin.local.password) 
 		return challengeuserlogin.save(function(err, user){
 		    //return challengeuserlogin.save().then(function(err, user){
 		    if(err){
