47d6620cb0b2ff319c94723ae3a59e8c32a35652
aiton goldman <yanetut@gmail.com>
2015-11-27 21:20:34 +0000

    added util to populate db through curl

---
+++ curl_populate.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -X POST -d '{"local":{"email":"me@me.com","password":"hithere","username":"mememe"}, "region":"pittsburgh","displayName":"mydisplay"}' http://localhost:3000/user
