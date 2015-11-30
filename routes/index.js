323c96d597f2ce3955b4c58254308dc8b262c8ed
aiton goldman <yanetut@gmail.com>
2015-11-30 00:58:42 +0000

    added getuserinfo and user editing routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var express = require('express');
 require('../models/ChallengeUserLogin');
 
 var ChallengeUserLogin = mongoose.model('ChallengeUserLogin');    
+var ChallengeScore = mongoose.model('ChallengeScore');    
+
+var ObjectId = require('mongoose').Types.ObjectId; 
 
 var router = express.Router();
 
@@ -13,13 +16,17 @@ module.exports = function(app, app_configuration){
     var score_controllers = require('./score_controllers')(app_configuration.scoreType)
 
     router.post('/user', user_controllers.addUser);
+    router.put('/user/:userid/email', user_controllers.updateUserEmail);
+    router.put('/user/:userid/displayname', user_controllers.updateUserDisplayName);
     router.post('/score', score_controllers.addScore);
-    router.get('/test',function(req,res,next){
-	query = ChallengeUserLogin.find().distinct('displayName', function(err,data){
-	    ChallengeUserLogin.find().in('displayName',data).sort('displayName').limit(data.length).exec(function(err,data){
-		res.json({result:true});
-		return true		
-	    })	    
+    router.get('/user/:userid', user_controllers.getUserInfo);
+    router.get('/test/:userid',function(req,res,next){
+	//console.log(req.params.userid)
+	query = ChallengeScore.find().where('scorePlayers.playerId').in(new ObjectId(req.params.userid)).sort('-dateOfScore').limit(1).exec(function(err,data){
+	    // ChallengeUserLogin.find().in('displayName',data).sort('displayName').limit(data.length).exec(function(err,data){
+	     	res.json({result:data});
+	     	return true		
+	    // })	    
 	})
     });
     return router;
--- routes/user_controllers.js
+++ routes/user_controllers.js
@@ -1,27 +1,96 @@
 
 function outputValidationError(err){
-    error_message = "VALIDATION ERROR WHILE CREATING NEW USER : "
+    error_message = "VALIDATION ERROR WHILE PERFORMING USER OPERATION : "
     for(i in err.errors){
 	error_message = error_message + ('**'+i+"--"+err.errors[i].name+"--"+err.errors[i].message)
     }
     return error_message
 }
+
+function report_general_error(type_of_error,error, res){
+    res.status(400).send({result: false,
+			  error: type_of_error+' : '+error 
+			 })
+    return true
+}
+
+function report_mongo_error(err, res){
+    if(err){
+	res.status(400).send({result: false,
+			      error: outputValidationError(err)
+			     })
+	return true
+    }
+    return false
+}
+
 module.exports = function(login_type){
     var mongoose = require('mongoose');
     require('../models/ChallengeUserLogin');
+    require('../models/ChallengeScore');
     var ChallengeUserLogin = mongoose.model('ChallengeUserLogin');
+    var ChallengeScore = mongoose.model('ChallengeScore');    
+    var ObjectId = require('mongoose').Types.ObjectId; 
+
     controllers = {};
     if(login_type == 'local'){
 	controllers = {
+	    updateUserEmail: function(req,res,next){		
+		new_email = req.body.email;
+		if(new_email === undefined){
+		    new_email='';
+		}
+		return ChallengeUserLogin.update({_id:req.params.userid},{'local.email':new_email},{runValidators: true}).exec(function(err){
+		    if(report_mongo_error(err, res)){
+			return false
+		    }
+		    res.json({result:true,
+			     error:err});
+		    return true;
+		})
+	    },
+	    updateUserDisplayName: function(req,res,next){
+		new_displayname = req.body.displayName;
+		if(new_displayname === undefined){
+		    new_displayname='';
+		}
+		return ChallengeUserLogin.update({_id:req.params.userid},{'displayName':new_displayname}, {runValidators: true}).exec(function(err){
+		    if(report_mongo_error(err, res)){
+			return false
+		    }
+
+		    res.json({result:true,
+			     error: err});
+		    return true;
+		})
+	    },
+	    getUserInfo: function(req,res,next){
+		return ChallengeScore.find().where('scorePlayers.playerId').in(new ObjectId(req.params.userid)).sort('-dateOfScore').limit(1).exec(function(err,latest_score){
+		    if(report_mongo_error(err, res)){
+			return false
+		    }
+		    return ChallengeUserLogin.findOne({_id:new ObjectId(req.params.userid)}, function(err,user){
+			var latest_score_player_index = -1;
+			if(latest_score[0].scorePlayers[0].playerId == user._id){
+			    latest_score_player_index = 0
+			} else {
+			    latest_score_player_index = 1
+			}
+			plain_user = user.toObject()
+			delete plain_user.local.password;
+
+			plain_user.wins = latest_score[0].scorePlayers[latest_score_player_index].wins + 1
+			res.json({result:plain_user})
+		    })
+		})
+
+	    },
 	    addUser: function(req,res,next){
 		var challengeuserlogin = new ChallengeUserLogin(req.body);
 		challengeuserlogin.local.password = challengeuserlogin.generateHash(challengeuserlogin.local.password) 
 		return challengeuserlogin.save(function(err, user){
 		    //return challengeuserlogin.save().then(function(err, user){
-		    if(err){
-			res.status(400).send({result: false,
-					      error: outputValidationError(err)
-					     })
+		    if(report_mongo_error(err, res)){
 			return false
 		    }
 		    res.json({result:user._id});
